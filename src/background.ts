8cbf9b2a5e98c8504253970ca1322afc207169ad
jeffreyjyc <jeffreyjyc@gmail.com>
2026-05-10 00:57:07 -0400

    Delete src directory

--- src/background.ts
+++
@@ -1,6 +0,0 @@
-// Background script to handle side panel behavior
-if (typeof chrome !== 'undefined' && chrome.sidePanel) {
-  chrome.sidePanel
-    .setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error) => console.error(error));
-}
